0e8e61b0866e81555bd2ed0e8f55ccbdc1cf826a
balajidevtar5 <161289674+balajidevtar5@users.noreply.github.com>
2024-07-19 15:29:42 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 function App() {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
   const [showInstallButton, setShowInstallButton] = useState(false);
+  console.log("showInstallButton",showInstallButton);
 
   useEffect(() => {
     const handler = (e) => {
